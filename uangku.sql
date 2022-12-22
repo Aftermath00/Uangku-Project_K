100a101ef98df1f6f27484fadc1813a4fdf36bca
raflyritonga <ritongarafly21@gmail.com>
2022-12-22 23:06:52 +0700

    add exporting sql

---
+++ uangku.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 22, 2022 at 05:05 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uangku`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pemasukan`
+--
+
+CREATE TABLE `pemasukan` (
+  `id_pemasukan` int(11) NOT NULL,
+  `tgl_pemasukan` date NOT NULL,
+  `ket_pemasukan` varchar(255) NOT NULL,
+  `nominal_pemasukan` int(11) NOT NULL,
+  `kategori_pemasukan` varchar(50) NOT NULL,
+  `username_pemasukan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pemasukan`
+--
+
+INSERT INTO `pemasukan` (`id_pemasukan`, `tgl_pemasukan`, `ket_pemasukan`, `nominal_pemasukan`, `kategori_pemasukan`, `username_pemasukan`) VALUES
+(0, '2022-12-09', 'Upah Nguli', 1000000, '', 'admin'),
+(1, '2022-12-07', 'upah', 100000, '', 'dinah'),
+(10, '2022-12-07', 'Gaji Admin', 400000, '', 'admin'),
+(12, '2022-12-02', 'Dikasih Orangtua', 200000, 'Assets', 'jamal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengeluaran`
+--
+
+CREATE TABLE `pengeluaran` (
+  `id_pengeluaran` int(11) NOT NULL,
+  `tgl_pengeluaran` date NOT NULL,
+  `ket_pengeluaran` varchar(255) NOT NULL,
+  `nominal_pengeluaran` int(11) NOT NULL,
+  `kategori_pengeluaran` varchar(50) NOT NULL,
+  `username_pengeluaran` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengeluaran`
+--
+
+INSERT INTO `pengeluaran` (`id_pengeluaran`, `tgl_pengeluaran`, `ket_pengeluaran`, `nominal_pengeluaran`, `kategori_pengeluaran`, `username_pengeluaran`) VALUES
+(9, '2022-12-09', 'Beli Bakso', 30000, 'Food', 'jamal'),
+(10, '2022-12-13', 'Beli Sepatu', 200000, 'Clothing', 'jamal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profil`
+--
+
+CREATE TABLE `profil` (
+  `id_profil` int(11) NOT NULL,
+  `nama_lengkap` varchar(255) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `pekerjaan` varchar(255) NOT NULL,
+  `batas_pengeluaran` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `profil`
+--
+
+INSERT INTO `profil` (`id_profil`, `nama_lengkap`, `tanggal_lahir`, `jenis_kelamin`, `pekerjaan`, `batas_pengeluaran`, `email`) VALUES
+(1, 'Jamaludin Siregar', '2022-12-01', 'Male', 'Pekerja Lepas', 200000, 'jamal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_username` varchar(255) DEFAULT NULL,
+  `user_password` varchar(255) DEFAULT NULL,
+  `user_email` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_username`, `user_password`, `user_email`) VALUES
+(1, 'admin', '0192023a7bbd73250516f069df18b500', 'admin123@gmail.com'),
+(5, 'dinah', 'fdbe76347aa5b0251377256ad20e5a93', 'dinah'),
+(10, 'jamal', '74f56399c89f4bd03ff5e85b6bf4e85f', 'jamal');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pemasukan`
+--
+ALTER TABLE `pemasukan`
+  ADD PRIMARY KEY (`id_pemasukan`);
+
+--
+-- Indexes for table `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  ADD PRIMARY KEY (`id_pengeluaran`);
+
+--
+-- Indexes for table `profil`
+--
+ALTER TABLE `profil`
+  ADD PRIMARY KEY (`id_profil`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_username` (`user_username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pemasukan`
+--
+ALTER TABLE `pemasukan`
+  MODIFY `id_pemasukan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `pengeluaran`
+--
+ALTER TABLE `pengeluaran`
+  MODIFY `id_pengeluaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `profil`
+--
+ALTER TABLE `profil`
+  MODIFY `id_profil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

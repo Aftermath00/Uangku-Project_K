fb7e525950efdc47314133a9bba7f13eeb3886db
raflyritonga <ritongarafly21@gmail.com>
2022-12-23 11:20:51 +0700

    adding exporting sql file

--- uangku.sql
+++ uangku.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2022 at 05:05 PM
+-- Generation Time: Dec 23, 2022 at 05:19 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.4.25
 
